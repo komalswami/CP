670347c9581362930fca6d82047706978b1ec495
komal swami <46648301+komalswami@users.noreply.github.com>
2020-09-03 15:39:34 +0530

    Update mine.cpp

--- mine.cpp
+++ mine.cpp
@@ -2,7 +2,18 @@
 #include<vector>
 using namespace std;
 char a[100][100];
-int rows,columns;
+int rows,cols;
+int isinside(int i,int j)
+{
+    return(i>=0 && i<rows && j>=0 && j<cols);
+}
+
+void increment(int i,int j)
+{
+    if(isinside(i,j) && a[i][j]!=-1)
+        a[i][j]++;
+}
+
 void setmine(int i,int j)
 {
     a[i][j]=-1;
@@ -10,22 +21,14 @@ void setmine(int i,int j)
         for(int n=-1;n<1;n++)
             increment(j+m,j+n);
 }
-void increment(int i,int j)
-{
-    if(isinside(i,j) && a[i][j]!=-1)
-        a[i][j]++;
-}
-int isinside(i,j)
-{
-    return(i>=0 && i<rows && j>=0 && j<cols);
-}
+
 void printres()
 {
     for(int i=0;i<rows-1;i++)
     {
         for(int j=0;j<cols-1;j++)
         {
-            x=a[i][j];
+            char x=a[i][j];
             if(x==-1)
                 cout<<"*";
             else
@@ -35,15 +38,16 @@ void printres()
 }
 int main()
 {
-    cin>>rows>>columns;
+    cin>>rows>>cols;
     int i,j;
     a[i][j]=0;
     for(i=0;i<rows-1;i++)
     {
+        
         for(j=0;j<cols-1;j++)
         {
-            char currentchar=a[j];
-            if(currentchar="*")
+            char currentchar=cin>>a[i][j];
+            if(currentchar=="*")
                 setmine(i,j);
         }
     }
