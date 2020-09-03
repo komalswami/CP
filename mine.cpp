0d3bf3ce8b2ad67cdf0f8b5da0ab5c68ee352c8e
komal swami <46648301+komalswami@users.noreply.github.com>
2020-09-04 00:31:07 +0530

    Update mine.cpp

--- mine.cpp
+++ mine.cpp
@@ -3,52 +3,90 @@
 using namespace std;
 int a[100][100];
 int rows,cols;
+void clearField()
+{
+int i,j;
+for(i=0;i<rows;i++){
+for(j=0;j<cols;j++)
+{
+a[i][j]=0;
+}
+}
+}
+void setbound(int r,int c)
+{
+rows=r;
+cols=c;
+}
 int isinside(int i,int j)
 {
-    return(i>=0 && i<rows && j>=0 && j<cols);
+return i>=0 && i<rows && j>=0 && j<cols;
 }
-
 void increment(int i,int j)
 {
-    if(isinside(i,j) && a[i][j]!=-1)
-        a[i][j]++;
+if(isinside(i,j)&&a[i][j]!=-1)
+++a[i][j];
 }
-
 void setmine(int i,int j)
 {
-    a[i][j]=-1;
-    for(int m=-1;m<1;m++)
-        for(int n=-1;n<1;n++)
-            increment(j+m,j+n);
+a[i][j]=-1;
+int m,n;
+for(m=-1;m<=1;m++)
+{
+for(n=-1;n<=1;n++)
+{
+increment(i+m,j+n);
+}
+}
 }
 
 void printres()
 {
-    for(int i=0;i<rows-1;i++)
-    {
-        for(int j=0;j<cols-1;j++)
-        {
-            char x=a[i][j];
-            if(x==-1)
-                cout<<"*";
-            else
-                cout<<x;
-        }
-    }
+int i,j;
+for(int i=0;i<rows;i++)
+{
+for(int j=0;j<cols;j++)
+{
+int x=a[i][j];
+if(x==-1)
+cout<<"*";
+else
+cout<<x;
 }
+cout<<endl;
+}
+}
+
 int main()
 {
-    cin>>rows>>cols;
-    int i,j;
-    a[i][j]=0;
-    for(i=0;i<rows-1;i++)
-    {
-        for(j=0;j<cols-1;j++)
-        {
-            cin>>a[i][j];
-            if(a[i][j]=='*')
-                setmine(i,j);
-        }
-    }
-    printres();
+int r,c;
+for(;;)
+{
+cin>>r>>c;
+if(r==0 && c==0)
+{
+break;
+}
+setbound(r,c);
+clearField();
+
+int i;
+for(i=0;i<r;i++){
+char input[c+1];
+cin>>input;
+
+int j;
+for(j=0;j<c;j++)
+{
+char currentChar=input[j];
+if(currentChar=='*')
+{
+setmine(i,j);
+}
+}
+}
+printres();
+
+}
+return 0;
 }
