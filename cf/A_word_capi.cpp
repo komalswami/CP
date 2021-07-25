10d08b92a005af4f3ea11a0ed5bba2b85d16286f
komal swami <46648301+komalswami@users.noreply.github.com>
2021-07-25 22:36:11 +0530

    Create A_word_capi.cpp

---
+++ cf/A_word_capi.cpp
@@ -0,0 +1,15 @@
+    #include<iostream>
+    #include<bits/stdc++.h>
+    #include <string>
+    using namespace std;
+    int main()
+    {
+        string one;
+        cin>>one;
+        
+      
+        one[0]=toupper(one[0]);
+        std::cout << one << std::endl;
+        
+        return 0;
+    }
